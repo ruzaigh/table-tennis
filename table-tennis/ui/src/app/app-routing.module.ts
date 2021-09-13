38c09558769c5431e9dbee0e5a7c9aee529817fc
ruzaigh <ruzaigh.kalam2@gmail.com>
2021-09-13 21:23:14 +0200

    busy setting up the login

--- table-tennis/ui/src/app/app-routing.module.ts
+++ table-tennis/ui/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './pages/admin/admin.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/sign-up', pathMatch: 'full'},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'sign-up', component: SignUpComponent},
+  {path: 'login', component: LoginComponent},
+  {path: "admin", component: AdminComponent},
+  {path: "home", component: HomeComponent}
 ];
 
 @NgModule({
--- table-tennis/ui/src/app/app.module.ts
+++ table-tennis/ui/src/app/app.module.ts
@@ -4,15 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { Router, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    LoginComponent,
+    HomeComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'admin',
+        component: AdminComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
